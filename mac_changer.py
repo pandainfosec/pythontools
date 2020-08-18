6c9d2f86c68be1711f2724bc2e5c84d5bb2693ff
Pravinkumar Singh <43314676+pandainfosec@users.noreply.github.com>
2020-08-18 20:14:12 +0530

    Create mac_changer.py

---
+++ mac_changer.py
@@ -0,0 +1,11 @@
+import os 
+import subprocess        #subprocess is a module to execute os commands
+
+
+interface = input("specify the interface > ")
+mac_address = input("enter the mac address > ")
+
+
+subprocess.call("ifconfig "+interface+" down", shell=True)
+subprocess.call("ifconfig eth0 ether "+mac_address, shell=True)
+subprocess.call("ifconfig "+interface+" up", shell=True)
